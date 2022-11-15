d6530a4e48628d51ea094d0cf48d43f8b09fc785
VictorWKey <victorwkey@gmail.com>
2022-11-14 23:27:24 -0600

    Generics learned

--- notes.ts
+++ notes.ts
@@ -256,3 +256,55 @@ console.log(user);
 // }
 
 // console.log(user2);
+
+
+
+"GENERICS"
+
+//Es codigo reutilizable que funciona con multiples tipos
+//Pueden ser funciones, intefaces o clases
+
+// Por lo general se representan con una <T>
+
+// Por lo general se usa para reutilizar clases en los casos que se ocupe utilizar clases similares, ya que no tiene sentido copiar y pegar casi el mismo codigo
+
+interface Person {
+    id: number;
+    name: string;
+}
+
+interface Employee extends Person {
+    role: string;
+}
+
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+}
+
+interface Data<T> {
+    addItem(newItem: T): void;
+}
+
+class DataCollection<T extends {id: number, name: string}> implements Data<T> {
+    private items: T[] = [];
+
+    addItem(newItem: T): void {
+        this.items.push(newItem);
+    }
+
+    getItems(): void {
+        console.log(`List of items`, JSON.stringify(this.items));
+    }
+
+    getNames(): string [] {
+        return this.items.map((item) => item.name);
+    }
+
+    getItemById(id: number): T | undefined {
+        return this.items.find((item: T) => item.id == id)
+    }
+} // El extends en el generic sirve por si acaso se quiere acceder a propiedades de objetos que aun no estan definidas 
+
+const productCollection = new DataCollection<Product>();
